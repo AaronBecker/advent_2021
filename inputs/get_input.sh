e2ac9468ad28eef6344d442ef3395e8d53287314
Aaron Becker <akb@falsesharing.com>
2021-12-11 20:26:03 -0800

    Don't re-download input files that exist already.

--- inputs/get_input.sh
+++ inputs/get_input.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-SESSION=`cat session_cookie.txt`
-curl https://adventofcode.com/2021/day/$1/input --cookie "session=$SESSION" > ./$1
+
+if [ ! -f "$1"]; then
+    SESSION=`cat session_cookie.txt`
+    curl https://adventofcode.com/2021/day/$1/input --cookie "session=$SESSION" > $1
+fi
