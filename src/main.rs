b1bebfb92228375b3ef3ecf415a5486438dbe39c
Aaron Becker <akb@falsesharing.com>
2021-12-11 21:40:59 -0800

    Add boilerplate for accepting command line arguments and loading input contents.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn problem_input(input_num: i32) -> String {
+    
+    let inpath = Path::new(env!("CARGO_MANIFEST_DIR")).join("inputs").join(format!("{}", input_num));
+    return fs::read_to_string(inpath.clone()).expect(format!("Unable to read file {}", inpath.display()).as_str());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: {} <input number>", args[0]);
+        return
+    }
+    let input_num = args[1].parse::<i32>().expect(format!("Couldn't parse {} as a problem number.", args[1]).as_str());
+    println!("{}", problem_input(input_num));
 }
--- target/debug/deps/advent_2021.d
+++ target/debug/deps/advent_2021.d
@@ -3,3 +3,5 @@ C:\Users\akbec\Documents\Projects\advent_2021\target\debug\deps\advent_2021.exe:
 C:\Users\akbec\Documents\Projects\advent_2021\target\debug\deps\advent_2021.d: src\main.rs
 
 src\main.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=C:\\Users\\akbec\\Documents\\Projects\\advent_2021
