6c1029782c99a2035730a2e8d30f862ec34047bb
Aaron Becker <akb@falsesharing.com>
2021-12-11 22:13:35 -0800

    Add simple function dispatch

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-fn problem_input(input_num: i32) -> String {
-    
+fn day_1(input: String) {
+    println!("{}", input.len());
+}
+
+fn problem_input(input_num: usize) -> String {
     let inpath = Path::new(env!("CARGO_MANIFEST_DIR")).join("inputs").join(format!("{}", input_num));
     return fs::read_to_string(inpath.clone()).expect(format!("Unable to read file {}", inpath.display()).as_str());
 }
@@ -14,6 +17,11 @@ fn main() {
         println!("usage: {} <input number>", args[0]);
         return
     }
-    let input_num = args[1].parse::<i32>().expect(format!("Couldn't parse {} as a problem number.", args[1]).as_str());
-    println!("{}", problem_input(input_num));
+    let input_num = args[1].parse::<usize>().expect(format!("Couldn't parse {} as a problem number.", args[1]).as_str());
+    let funcs = vec![day_1];
+    if input_num < funcs.len() {
+        println!("Can't find function for input {}", input_num);
+        return
+    }
+    funcs[input_num-1](problem_input(input_num))
 }
